7d05d63cc33b626a076b7ce9f44c55160fa751aa
Alexander Eberspächer <alex.eberspaecher@googlemail.com>
2012-11-23 19:14:50 +0100

    Add zbiry() warpper to amos module

--- src/amos.f90
+++ src/amos.f90
@@ -1,10 +1,14 @@
 module amos
   implicit none
 
-  ! this module contains interfaces for the amos routines (in unmodified form
+  ! This module contains interfaces for the amos routines (in unmodified form
   ! found on http://netlib.org/amos).
   ! The slightly modified sources taken from SciPy's "legacy/amos/special"
   ! module are linked against.
+
+  ! The original code was written by Donald E. Amos at Sandia National Labratories
+  ! See also "Portable package for Bessel functions of a complex argument and
+  ! non-negative order" by D. E. Amos in Trans. Math. Software (1986)
 
   ! Note that we made all routines pure routines. We do this to be able to
   ! use these routines in OpenMP loops and forall statements. The F77 routines
@@ -13,8 +17,7 @@ module amos
   ! Technically, there is one exception: the ?1mach.f90 routines (used in
   ! finding machine constants) each contain a write statement in case the query
   ! fails. We ignore this possibility (as we trust the queries made by amos).
-
-  ! TODO: note on the *mach.f90 routines
+  ! The author of the wrapper uses this without any problems in production code.
 
   interface
 
@@ -30,7 +33,7 @@ module amos
 
      ! Bessel Y function of second kind:
      pure subroutine zbesy(re, im, order, scaling, length, zOut_r, zOut_i, &
-        underflows, workr, worki, ierr)
+          underflows, workr, worki, ierr)
        implicit none
        double precision, intent(in) :: re, im, order
        integer, intent(in) :: scaling, length
@@ -69,15 +72,23 @@ module amos
        integer, intent(out) :: underflows, ierr
      end subroutine zbesh
 
-     ! Airy function (or its derivative):
-     pure subroutine zairy(re, im, deriv, scaling, zOut_r, zOut_i, &
-          underflow, ierr)
+     ! Airy function Ai (or its derivative):
+     pure subroutine zairy(re, im, deriv, scaling, zOut_r, zOut_i, underflow, ierr)
        implicit none
        double precision, intent(in) :: re, im
        integer, intent(in) :: deriv, scaling
        double precision, intent(out) :: zOut_r, zOut_i
        integer, intent(out) :: underflow, ierr
      end subroutine zairy
+
+     ! Airy function Bi (or its derivative)
+     pure subroutine zbiry(re, im, deriv, scaling, zOut_r, zOut_i, underflow, ierr)
+       implicit none
+       double precision, intent(in) :: re, im
+       integer, intent(in) :: deriv, scaling
+       double precision, intent(out) :: zOut_r, zOut_i
+       integer, intent(out) :: underflow, ierr
+     end subroutine zbiry
 
   end interface
 
