a87c6d676dac19902f42f68ea177527e1e666581
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2016-10-11 21:08:35 -0300

    Add secant method to optimize

--- src/optimize.f90
+++ src/optimize.f90
@@ -6,7 +6,7 @@ use types, only: dp
 use utils, only: stop_error
 implicit none
 private
-public bisect
+public bisect,secant
 
 interface
     real(dp) function func(x)
@@ -43,5 +43,54 @@ do while (b_ - a_ > tol)
 end do
 c = (a_ + b_)/2
 end function
+
+real(dp) function secant(f, a,b,tol, maxiter) result (c)
+!Solves f(x) = 0 on using the a and b as starting values
+procedure(func) :: f
+real(dp),intent(in) :: a,b,tol
+integer,optional :: maxiter
+
+integer :: maxiter_
+real(dp) :: xstart, xnext
+real(dp) :: fstart, fnext
+integer ::  i
+
+if(present(maxiter)) then
+    maxiter_ = maxiter
+else
+    maxiter_ = 100
+endif
+
+
+xstart = a
+xnext = b
+fstart = f(xstart)
+if(abs(fstart ) < tiny(1.0_dp)) then
+    c = xstart
+    return
+endif
+fnext = f(xnext)
+if(abs(fnext ) < tiny(1.0_dp)) then
+    c = xnext
+    return
+endif
+
+do i = 1, maxiter_
+    
+    if( abs(fnext - fstart) < tiny(1.0_dp)) then
+       call stop_error("secant: division by zero")
+    endif
+    c = (xstart*fnext - xnext*fstart)/(fnext - fstart)
+    if (abs(c - xnext) < tol) return ! root found 
+    !update variables
+    xstart = xnext
+    fstart = fnext
+    xnext = c
+    fnext = f(c)
+enddo
+!max iterations number reached
+call stop_error("secant: method did not converge")
+end function secant
+    
 
 end module
