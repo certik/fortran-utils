86a358b67cc5ca16709e28b848d3f3b85ef9364f
Ondřej Čertík <ondrej@certik.us>
2019-10-15 11:24:56 -0700

    Add a test

--- tests/linalg/test_cholesky.f90
+++ tests/linalg/test_cholesky.f90
@@ -1,11 +1,11 @@
 program test_cholesky
 use types, only: dp
 use utils, only: assert
-use linalg, only: cholesky
+use linalg, only: cholesky, solve, solve_triangular
 implicit none
 
 real(dp), parameter :: eps = 1e-12_dp
-real(dp) :: A(3,3), A2(3,3), L(3,3)
+real(dp) :: A(3,3), A2(3,3), L(3,3), b(3), x1(3), x2(3)
 
 A = reshape([4, 12, -16, 12, 37, -43, -16, -43, 98], shape=[3,3], order=[2,1])
 L = cholesky(A)
@@ -14,5 +14,12 @@ print *, A
 print *, L
 print *, A2
 call assert(maxval(abs(A - A2)) < eps)
+
+b = [1, 2, 3]
+x1 = solve(A, b)
+print *, x1
+x2 = solve_triangular(L, solve_triangular(L, b), .true.)
+print *, x2
+call assert(maxval(abs(x1 - x2)) < eps)
 
 end program
